3f641738e7cced6650c5aa6f19f04741c634e5a0
Maria-Michail <maria.magdalena.michail@gmail.com>
2020-11-10 14:59:38 +0100

    try

---
+++ Database/Data/DataModel.cs
@@ -0,0 +1,8 @@
+namespace Database.DataModel
+{
+    public class DataModel{
+        public static string getUsername(){
+            return "Maria";
+        }
+    }
+}
---
+++ Database/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Database.Model;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataModel dataModel = new DataModel();
+            Server server = new Server(dataModel);
+            server.start();
+        }
+    }
+}
