278f3a9c59213609435814aac6c42d4630958858
SprytnyMarek <skrzypkowski.pawel@wp.pl>
2020-11-11 12:12:21 +0100

    sqlite database

--- WebApi/Data/Models/User.cs
+++ WebApi/Data/Models/User.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Models
 {
     public class User{
+        [Key, MaxLength(15)]
         public string username{get;set;}
+        
+        [Required, MaxLength(15)]
         public string password{get;set;}
 
 
